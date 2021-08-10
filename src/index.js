973ed0db4171735cd299ea9945b0bb8376314be3
zzq666897 <zzq666897@gmail.com>
2021-08-10 18:12:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,9 +35,10 @@ function preload()
     //contains functions and properties we can use
     
     this.load.image('sky','assets/sky.png');
+    this.load.image('bird', 'assets/bird.png');
 }
 
-
+let bird = null;
 
 function create()
 {
@@ -50,7 +51,10 @@ function create()
      //this.add.image(config.width / 2, config.height / 2,'sky');
 
      this.add.image(0, 0,'sky').setOrigin(0,0);
-     this.add.sprite(config.width / 2, config.height / 2, 'bird').setOrigin(0);
+    
+     bird = this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+
+     debugger
 }
 
 //...
